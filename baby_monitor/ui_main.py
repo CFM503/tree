426d67c946a19d975e185ad5a854383cac8ff55f
CFM503 <pzeus@163.com>
2026-05-27 10:45:24 +0800

    Bump version to v2.2-stable: Release stable version with customizable network timeout

--- baby_monitor/ui_main.py
+++ baby_monitor/ui_main.py
@@ -499,7 +499,7 @@ class MainWindow(QMainWindow):
         self._status_bar.showMessage("请选择宝宝")
 
         # 软件版本号
-        self._status_version = QLabel("v2.2")
+        self._status_version = QLabel("v2.2-stable")
         self._status_version.setStyleSheet("color: #777; margin-right: 15px; font-weight: bold;")
         self._status_bar.addPermanentWidget(self._status_version)
 
